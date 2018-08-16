356c2303a556e511c250000b4c85be23c84d2904
Lucas Costa <lucasfrct@users.noreply.github.com>
2018-08-16 10:03:05 -0300

    ignorar

--- index.php
+++ index.php
@@ -5,16 +5,18 @@ include_once ( "layout.php" );
 #newHost ( "domain", "c:\Server\htdocs\path" );
 newHost ( "localhost", "c:\Server\htdocs" );
 newHost ( "components", "c:\Server\htdocs\components" );
-newHost ( "callcommunity", "c:\Server\htdocs\callcommunity" );
-newHost ( "corebank", "c:\Server\htdocs\core-banking-client" );
-newHost ( "crud", "c:\Server\htdocs\crud" );
+//newHost ( "callcommunity", "c:\Server\htdocs\callcommunity" );
+//newHost ( "corebank", "c:\Server\htdocs\core-banking-client" );
+//newHost ( "crud", "c:\Server\htdocs\crud" );
+newHost ( "soundapp", "c:\Server\htdocs\soundapp" );
 newHost ( "ligthcontroler", "c:\Server\htdocs\web-light-controller" );
 
 #addLink ( "Name", "url" );
 addLink ( "Components", "http://components/" );
-addLink ( "Call Community", "http://callcommunity/" );
-addLink ( "Core Bank Client", "http://corebank/" );
-addLink ( "CRUD", "http://crud/" );
+//addLink ( "Call Community", "http://callcommunity/" );
+//addLink ( "Core Bank Client", "http://corebank/" );
+//addLink ( "CRUD", "http://crud/" );
+addLink ( "Sound App", "http://soundapp/" );
 addLink ( "Web Light Controller", "http://ligthcontroler/" );
 
 #report ( );
