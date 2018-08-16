7133da05c788e50a7d9613d62c3f3b6803258b43
Lucas Costa <lucasfrct@users.noreply.github.com>
2018-08-16 10:04:13 -0300

    Revert "ignorar"
    This reverts commit 356c2303a556e511c250000b4c85be23c84d2904.

--- index.php
+++ index.php
@@ -5,18 +5,16 @@ include_once ( "layout.php" );
 #newHost ( "domain", "c:\Server\htdocs\path" );
 newHost ( "localhost", "c:\Server\htdocs" );
 newHost ( "components", "c:\Server\htdocs\components" );
-//newHost ( "callcommunity", "c:\Server\htdocs\callcommunity" );
-//newHost ( "corebank", "c:\Server\htdocs\core-banking-client" );
-//newHost ( "crud", "c:\Server\htdocs\crud" );
-newHost ( "soundapp", "c:\Server\htdocs\soundapp" );
+newHost ( "callcommunity", "c:\Server\htdocs\callcommunity" );
+newHost ( "corebank", "c:\Server\htdocs\core-banking-client" );
+newHost ( "crud", "c:\Server\htdocs\crud" );
 newHost ( "ligthcontroler", "c:\Server\htdocs\web-light-controller" );
 
 #addLink ( "Name", "url" );
 addLink ( "Components", "http://components/" );
-//addLink ( "Call Community", "http://callcommunity/" );
-//addLink ( "Core Bank Client", "http://corebank/" );
-//addLink ( "CRUD", "http://crud/" );
-addLink ( "Sound App", "http://soundapp/" );
+addLink ( "Call Community", "http://callcommunity/" );
+addLink ( "Core Bank Client", "http://corebank/" );
+addLink ( "CRUD", "http://crud/" );
 addLink ( "Web Light Controller", "http://ligthcontroler/" );
 
 #report ( );
