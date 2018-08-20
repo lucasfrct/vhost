bc19b95af399dcbccd603e40c2545fe278636bc0
Lucas Costa <lucasfrct@users.noreply.github.com>
2018-08-20 14:34:55 -0300

    App Virtual Hosts

--- index.php
+++ index.php
@@ -4,17 +4,21 @@ include_once ( "layout.php" );
 
 #newHost ( "domain", "c:\Server\htdocs\path" );
 newHost ( "localhost", "c:\Server\htdocs" );
-newHost ( "components", "c:\Server\htdocs\components" );
 newHost ( "callcommunity", "c:\Server\htdocs\callcommunity" );
+newHost ( "components", "c:\Server\htdocs\components" );
 newHost ( "corebank", "c:\Server\htdocs\core-banking-client" );
 newHost ( "crud", "c:\Server\htdocs\crud" );
+newHost ( "musicalsystematizer", "c:\Server\htdocs\musicalsystematizer" );
+newHost ( "soudapp", "c:\Server\htdocs\soundapp" );
 newHost ( "ligthcontroler", "c:\Server\htdocs\web-light-controller" );
 
 #addLink ( "Name", "url" );
-addLink ( "Components", "http://components/" );
 addLink ( "Call Community", "http://callcommunity/" );
+addLink ( "Components", "http://components/" );
 addLink ( "Core Bank Client", "http://corebank/" );
 addLink ( "CRUD", "http://crud/" );
+addLink ( "Musical Systematizer", "http://musicalsystematizer/" );
+addLink ( "Soud App", "http://soudapp/" );
 addLink ( "Web Light Controller", "http://ligthcontroler/" );
 
 #report ( );
