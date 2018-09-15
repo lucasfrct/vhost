b5ed69fbfd79d272ab3555fb39f407aeceb89be3
Lucas Costa <lucasfrct@gmail.com>
2018-09-15 18:20:57 -0300

    Add all repositories

--- index.php
+++ index.php
@@ -4,21 +4,27 @@ include_once ( "layout.php" );
 
 #newHost ( "domain", "c:\Server\htdocs\path" );
 newHost ( "localhost", "c:\Server\htdocs" );
+newHost ( "biblexml", "c:\Server\htdocs\bible-xml" );
 newHost ( "callcommunity", "c:\Server\htdocs\callcommunity" );
 newHost ( "components", "c:\Server\htdocs\components" );
 newHost ( "corebank", "c:\Server\htdocs\core-banking-client" );
 newHost ( "crud", "c:\Server\htdocs\crud" );
-newHost ( "musicalsystematizer", "c:\Server\htdocs\musicalsystematizer" );
-newHost ( "soudapp", "c:\Server\htdocs\soundapp" );
+newHost ( "lotodroid", "c:\Server\htdocs\lotodroid" );
+newHost ( "lotoweb", "c:\Server\htdocs\lotoweb" );
+newHost ( "musicalsystematizer", "c:\Server\htdocs\musical-systematizer" );
+newHost ( "soundapp", "c:\Server\htdocs\soundapp" );
 newHost ( "ligthcontroler", "c:\Server\htdocs\web-light-controller" );
 
 #addLink ( "Name", "url" );
+addLink ( "Bible XML", "http://biblexml/" );
 addLink ( "Call Community", "http://callcommunity/" );
 addLink ( "Components", "http://components/" );
 addLink ( "Core Bank Client", "http://corebank/" );
+addLink ( "Loto Droid", "http://lotodroid/" );
+addLink ( "Loto Web", "http://lotoweb/" );
 addLink ( "CRUD", "http://crud/" );
 addLink ( "Musical Systematizer", "http://musicalsystematizer/" );
-addLink ( "Soud App", "http://soudapp/" );
+addLink ( "Sound App", "http://soundapp/" );
 addLink ( "Web Light Controller", "http://ligthcontroler/" );
 
 #report ( );
