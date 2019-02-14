2787965249725cd9c523bff10519ba3237a9f123
Lucas Costa <lucasfrct@users.noreply.github.com>
2019-02-14 00:10:38 -0200

    add link

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+require_once ( "Vhost.php" );
+require_once ( "vhost.layout.php" );
+
+#$vhost->new (  /folder, domain, port );
+#addLink ( /folder, domain, port );
+
+$vhost = new VHost ( );
+$vhost->new (  "/", "localhost", "80" );
+
+$vhost->new (  "callcommunity", "callcommunity.dev.com", "80" );
+addLink ( "callcommunity", "callcommunity.dev.com", "80" );
+
+$vhost->new (  "components", "components.dev.com", "80" );
+addLink ( "components", "components.dev.com", "80" );
+
+$vhost->new (  "crud", "crud.dev.com", "80" );
+addLink ( "crud", "crud.dev.com", "80" );
