155e891da058bf10839e99c8a67a6c491a0acd3a
Lucas Costa <lucasfrct@users.noreply.github.com>
2019-03-02 11:21:54 -0300

    update

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ require_once ( "vhost.layout.php" );
 $vhost = new VHost ( );
 $vhost->new (  "/", "localhost", "80" );
 
-$vhost->new (  "callcommunity", "callcommunity.dev.com", "80" );
-addLink ( "callcommunity", "callcommunity.dev.com", "80" );
+#$vhost->new (  "callcommunity", "callcommunity.dev.com", "80" );
+#addLink ( "callcommunity", "callcommunity.dev.com", "80" );
 
 $vhost->new (  "components", "components.dev.com", "80" );
 addLink ( "components", "components.dev.com", "80" );
 
-$vhost->new (  "crud", "crud.dev.com", "80" );
-addLink ( "crud", "crud.dev.com", "80" );
+#$vhost->new (  "crud", "crud.dev.com", "80" );
+#addLink ( "crud", "crud.dev.com", "80" );
