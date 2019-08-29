3e79effe375d9e13caf2fb0cc5c7008d2a6341cd
Lucas Costa <lucasfrct@users.noreply.github.com>
2019-08-29 14:05:18 -0300

    updae link

--- index.php
+++ index.php
@@ -2,23 +2,21 @@
 require_once ( "Vhost.php" );
 require_once ( "vhost.layout.php" );
 
+# EXEMPLO
 #$vhost->new (  /folder, domain, port );
 #addLink ( /folder, domain, port );
 
 $vhost = new VHost ( );
+
+# LOCALHOST
 $vhost->new (  "/", "localhost", "80" );
 
-#$vhost->new (  "callcommunity", "callcommunity.dev.com", "80" );
-#addLink ( "callcommunity", "callcommunity.dev.com", "80" );
+$vhost->new ( "liber/app/web", "liber.dev.com", "80" );
+addLink ( "liber/app/web", "liber.dev.com", "80" );
 
-$vhost->new (  "components", "components.dev.com", "80" );
+$vhost->new ( "components", "components.dev.com", "80" );
 addLink ( "components", "components.dev.com", "80" );
 
-#$vhost->new (  "crud", "crud.dev.com", "80" );
-#addLink ( "crud", "crud.dev.com", "80" );
 
-$vhost->new (  "mvp-imoveis/", "mvpimoveis.dev.com", "80" );
-addLink ( "mvp-imoveis/", "mvpimoveis.dev.com", "80" );
-
-$vhost->new (  "mvp-marketing/", "mvpmarketing.dev.com", "80" );
-addLink ( "mvp-marketing/", "mvpmarketing.dev.com", "80" );
+$vhost->new ( "callcommunity", "callcommunity.dev.com", "80" );
+addLink ( "callcommunity", "callcommunity.dev.com", "80" );
